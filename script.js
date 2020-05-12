6102ee88870741169c2f7456a232ce13a609eb39
Yulia <yulia.aksenova5@gmail.com>
2020-05-11 23:30:48 -0600

    created

---
+++ script.js
@@ -0,0 +1,23 @@
+let body = document.body;
+fetch("https://api.github.com/users/YuliaEY")
+.then(res => res.json())
+.then(json => {
+	let h1 = document.createElement('h1');
+	document.body.appendChild(h1);
+	h1.innerHTML = json.name;
+
+	let div =document.createElement('div');
+	document.body.appendChild(div);
+	div.innerHTML = json.bio;
+	
+	let img = document.createElement('img');
+	document.body.appendChild(img);
+	img.src = json.avatar_url;
+
+	let div1 =document.createElement('div');
+	document.body.appendChild(div1);
+	div1.innerHTML = json.url;
+})
+.catch(err => console.log(err))
+
+
