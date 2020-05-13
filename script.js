8189094e412f0e520fa422043b8e2f59396f8041
Yulia <yulia.aksenova5@gmail.com>
2020-05-12 19:38:38 -0600

    Promise

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
-fetch("https://api.github.com/users/YuliaEY")
+
+setTimeout(()=>{
+	let spinner = document.getElementById('spinner');
+spinner.style.display="none";
+}, 3000);
+
+const getDate = new Promise((resolve, reject) => {
+ setTimeout(function() {
+ var now = new Date();
+document.body.innerHTML=(now);
+ }, 3000)
+})
+
+let userProfile = new Promise((resolve, reject) => {
+setTimeout(function() {
+const getUserProfile = fetch("https://api.github.com/users/YuliaEY")
 .then(res => res.json())
 .then(json => {
 	let h1 = document.createElement('h1');
@@ -19,10 +34,14 @@ fetch("https://api.github.com/users/YuliaEY")
 	link.href = json.html_url;
 	link.innerHTML="my profile"
 })
+}, 3000)
+})
+
+Promise.all([getDate, userProfile ])
 
 .catch(err => {
- 
-   console.error(err);
+    console.error(err);
    alert("Failed to find User");
 })
+
 
