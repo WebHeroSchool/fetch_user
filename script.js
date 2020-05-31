c1f1e52bcc3f48599f84c462dce07b46fa461393
Yulia <yulia.aksenova5@gmail.com>
2020-05-31 16:34:51 -0600

    updated with search

--- script.js
+++ script.js
@@ -5,43 +5,50 @@ setTimeout(()=>{
 spinner.style.display="none";
 }, 3000);
 
+let writeDate = () => {
+	 var now = new Date();
+	document.body.innerHTML=now;
+}
 const getDate = new Promise((resolve, reject) => {
- setTimeout(function() {
- var now = new Date();
-document.body.innerHTML=(now);
- }, 3000)
+	 setTimeout(function() {
+	  resolve(writeDate)
+	 }, 3000)
 })
-
+let query = window.location.search;
+let queryIndex =  query.indexOf("=");
+let username= query.slice(queryIndex+1);
 let userProfile = new Promise((resolve, reject) => {
-setTimeout(function() {
-const getUserProfile = fetch("https://api.github.com/users/YuliaEY")
-.then(res => res.json())
-.then(json => {
-	let h1 = document.createElement('h1');
-	document.body.appendChild(h1);
-	h1.innerHTML = json.name;
-
-	let div =document.createElement('div');
-	document.body.appendChild(div);
-	div.innerHTML = json.bio;
-	
-	let img = document.createElement('img');
-	document.body.appendChild(img);
-	img.src = json.avatar_url;
-
-	let link =document.createElement('a');
-	document.body.appendChild(link);
-	link.href = json.html_url;
-	link.innerHTML="my profile"
-})
-}, 3000)
+	setTimeout(function() {
+		const getUserProfile = fetch(`https://api.github.com/users/${username}`)
+		resolve(getUserProfile) 		
+	}, 3000)
 })
 
 Promise.all([getDate, userProfile ])
+	.then(([date, user]) => {
+		date();
+		return user.json()
+	} )
+	.then(json => {
+				let h1 = document.createElement('h1');
+				document.body.appendChild(h1);
+				h1.innerHTML = json.name;
 
-.catch(err => {
-    console.error(err);
-   alert("Failed to find User");
-})
+				let div =document.createElement('div');
+				document.body.appendChild(div);
+				div.innerHTML = json.bio;
+				
+				let img = document.createElement('img');
+				document.body.appendChild(img);
+				img.src = json.avatar_url;
+
+				let link =document.createElement('a');
+				document.body.appendChild(link);
+				link.href = json.html_url;
+				link.innerHTML="my profile";
+			})
+	.catch(err => console.error(err))
+
+
 
 
